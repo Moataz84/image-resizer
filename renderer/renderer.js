c9c364f3d3cb18e751b22a299fe16a3ef4e38b98
Moataz Ghazy <moatazghazy442005@outlook.com>
2023-11-23 21:42:36 -0500

    commit

--- renderer/renderer.js
+++ renderer/renderer.js
@@ -2,7 +2,8 @@ const msg = document.querySelector(".msg")
 
 document.querySelector(".image-upload input").addEventListener("change", e => {
   const file = e.target.files[0]
+  console.log(file);
   if (!file) return msg.innerText = "No file selected"
-  if (!file.type.includes("image")) return msg.innerText = "No file selected"
-  console.log("done");
+  if (!file.type.includes("image")) return msg.innerText = "File is not an image"
+  console.log("first")
 })
